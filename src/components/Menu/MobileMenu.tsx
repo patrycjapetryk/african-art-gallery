a8f8a2c4ce6f62a12ed6682fc42acba3e6ee4d6f
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2025-09-04 13:54:51 +0200

    Fix mobile menu error

--- src/components/Menu/MobileMenu.tsx
+++ src/components/Menu/MobileMenu.tsx
@@ -1,25 +1,19 @@
 import { type ComponentProps } from 'react';
-
 import { cn } from '@/utils/cn';
-import ButtonLanguage from '@/ui/ButtonLanguage';
-import Navigation from '@/ui/Navigation';
-import SocialMediaNavigation from '@/ui/SocialMediaNavigation';
-import SearchBar from '@/ui/SearchBar';
-
-import { menuData } from './menuData';
+import Navigation from '@/components/Navigation/Navigation';
+import SearchBar from '../SearchBar/SearchBar';
+import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
+import SocialMediaNavigation from '../SocialMediaNavigation/SocialMediaNavigation';
 
 type Props = ComponentProps<'div'>;
 
 export default function DesktopMenu({ className, ...spread }: Props) {
-  const { menuItems, menuLanguage, menuSocialMedia } = menuData;
-  const { label } = menuLanguage;
-
   return (
-    <div className={cn('flex w-full justify-end', className)} {...spread}>
-      <Navigation menuItems={menuItems} />
+    <div className={cn('flex w-full justify-end items-center gap-4', className)} {...spread}>
+      <Navigation />
       <SearchBar />
-      <SocialMediaNavigation menuSocialMedia={menuSocialMedia} />
-      <ButtonLanguage>{label}</ButtonLanguage>
+      <SocialMediaNavigation />
+      <LanguageSwitcher />
     </div>
   );
 }
